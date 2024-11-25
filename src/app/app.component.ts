5b3e396cec7894e8c794a6b4e62c0bdccfd7f4ef
Svetlin Ivanov <svetlinivnv@gmail.com>
2024-11-25 23:51:46 +0200

    add some screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { RegisterComponent } from './user/register/register.component';
 import { CartComponent } from './cart/cart.component';
-import { CatalogComponent } from './catalog/catalog.component';
+import { CatalogComponent } from './product/catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './user/register/register.component';
-import { CatalogComponent } from './catalog/catalog.component';
+import { CatalogComponent } from './product/catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { ProfileComponent } from './user/profile/profile.component';
+import { DetailsComponent } from './product/details/details.component';
+import { AddProductComponent } from './product/add-product/add-product.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'add-product', component: AddProductComponent },
   { path: 'catalog', component: CatalogComponent },
+  { path: 'product/:id', component: DetailsComponent },
   { path: 'cart', component: CartComponent },
   { path: 'profile', component: ProfileComponent },
   // User routing start
